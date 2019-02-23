346cda2ded8aeaee634ffcaffc9dd002d2ad0a20
senorcapitan <47877576+senorcapitan@users.noreply.github.com>
2019-02-22 23:01:28 -0500

    Add files via upload

---
+++ ikjb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int TOTALPOINTSEARNED;
+int TOTALPOINTSPOSSIBLE;
+int a;
+int main(void) {
+int assignments = scanf("how many different assignments?\n");
+int gradesEarned[assignments];
+int pointsPossible[assignments];
+}
+{
+  int numbers[assignments];
+  for(int i = 1; i<= assignments; i++){
+    numbers[i]=i
+  } printf("points earned for assignment[i]\n");
+
+for(int i=1; i<=assignments;i++){
+  printf("points possible for assignment[i]\n");
+}
+}
